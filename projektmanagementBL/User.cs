006c55dd70af3463df9791be8f43afdc5a1e5f3d
Daniel Patrick Studera <145760675+StuderaFHSTP@users.noreply.github.com>
2024-08-02 13:35:13 +0200

    User Klasse ausgefüllt

---
+++ projektmanagementBL/User.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projektmanagementBL
+{
+    internal class User
+    {
+        private String userID { get; set; }
+        private String name { get; set; }
+        private String surname { get; set; }
+        private String email { get; set; }
+        private String role { get; set; }
+        private String department { get; set; }
+        private String projectID { get; set; }
+
+        internal User() { }
+
+        public User(String userID, String name, String surname, String email, String role, String department, String projectID)
+        {
+            this.userID = userID;
+            this.name = name;
+            this.surname = surname;
+            this.email = email;
+            this.role = role;
+            this.department = department;
+            this.projectID = projectID;
+        }
+
+
+
+    }
+}
